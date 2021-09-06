5d017ee82dd4a21ede193d34581a47141c325174
Trung Ngoc <hongoctrungit@gmail.com>
2021-09-06 10:35:36 +0700

    update title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Trung Ngọc
         </p>
         <a
           className="App-link"
